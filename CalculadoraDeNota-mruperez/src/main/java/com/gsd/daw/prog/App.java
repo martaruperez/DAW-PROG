4883e3e642394e49fc8148b73b944ba40bf324df
m.ruperez <m.ruperez@2DAW-12.BUINF.local>
2024-01-22 15:57:56 +0100

    sgs

--- CalculadoraDeNota-mruperez/src/main/java/com/gsd/daw/prog/App.java
+++ CalculadoraDeNota-mruperez/src/main/java/com/gsd/daw/prog/App.java
@@ -5,10 +5,15 @@ public class App
     public static void main( String[] args )
     {
 		try {
-			Nota nota = new Nota(args);
+			Nota[] notas = new Nota [args.length];
+			for(int i = 0; i < args.length; i++) {
+				notas[i] = new Nota(args[i]);
+			}
 		} 
 		catch(IllegalArgumentException e) {
 			System.err.println("ERROR: Argumentos no validos");
+			return;
 		}
     }
+    
 }
--- CalculadoraDeNota-mruperez/src/main/java/com/gsd/daw/prog/Nota.java
+++ CalculadoraDeNota-mruperez/src/main/java/com/gsd/daw/prog/Nota.java
@@ -1,68 +1,21 @@
 package com.gsd.daw.prog;
 
 public class Nota {
-	private double[] notaExamenes = {-1};
-	private double[] notaPracticas = {-1};
-	private int[] notaActitud = {-1};
-	private int[] notaAsistencia = {-1};
+	private char tipoDeNota;
+	private double nota;
 	
-	public Nota(String[] args) throws IllegalArgumentException {
-		for(int i = 0; i < args.length; i++) {
-			almacenarNotas(args[i]);
-		}
-		if(!validar(args)) {
-			throw new IllegalArgumentException();
-		}
-	}
-	
-	private void almacenarNotas(String arg) {
-		// TODO: quitar esto de aqui y hacer un metodo de split
-		String[][] notaPartida = new String[] [2];
-		notaPartida = arg.split("-");
+	public Nota(String arg) throws IllegalArgumentException {
 		
-		switch (notaPartida[0]) {
-			case "A":
-				
-			break;
-			case "T":
-				almacenarNotasActitud (notaPartida[1]);
-			break;
-			case "C":
-				
-			break;
-			case "P":
-				
-			break;
-			default: 
-			
-			break;
-		}
 	}
 	
-	private void almacenarNotasActitud (String stringNota) {
-		if(this.notaActitud[0]==-1) {
-			this.notaActitud[0]=Integer.parseInt(stringNota);
-			return;
-		}
-		
-		int[] notaActitud = new int [this.notaActitud.length+1];
-		for(int i = 0; i < this.notaActitud.length; i++) {
-			notaActitud[i] = this.notaActitud[i];
-		}
-		notaActitud[this.notaActitud.length]=Integer.parseInt(stringNota);
-		this.notaActitud = notaActitud;
+	private String[] dividirEntrada(String arg){
+		return arg.split("-");
 	}
 	
-	private boolean validar(String[] args) {
+	private boolean validar(Nota[] notas) {
 		//que no haya menos de 4 notas (una por cada tipo)
-		if(args.length < 1) {
-			return false;
-		}
 		
 		//que no haya mas de 20 notas
-		if(args.length > 20) {
-			return false;
-		}
 		
 		//que haya al menos un examen
 		
@@ -71,21 +24,13 @@ public class Nota {
 		
 		
 		//que esté la nota de Actitud
-		if(this.notaActitud[0] == -1) {
-			return false;
-		}
 		
 		//que haya solo una nota de Actitud
-		if(this.notaActitud.length != 1) {
-			return false;
-		}
 		
 		//que esté la nota de Asistencia
 		
+		
 		//que haya solo una nota de Asistencia
-		if(this.notaAsistencia.length != 1) {
-			return false;
-		}
 		
 		//que las notas sean números
 		
@@ -94,25 +39,9 @@ public class Nota {
 		
 		
 		//que todas las notas vengan en el formato pedido [CPTA]-<numero-decimal>
+
 		
 		return true;
 	}
-	
-	public double[] getNotaExamenes() {
-		return this.notaExamenes;
-	}
-
-	public double[] getNotaPracticas() {
-		return this.notaPracticas;
-	}
-
-	public int[] getNotaActitud() {
-		return this.notaActitud;
-	}
-
-	public int[] getNotaAsistencia() {
-		return this.notaAsistencia;
-	}
-	
-	
+			
 }
