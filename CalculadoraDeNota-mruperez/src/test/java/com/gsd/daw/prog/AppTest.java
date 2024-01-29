b48d526e765233bed0e728d44ddd39c1b466dca2
m.ruperez <m.ruperez@2DAW-12.BUINF.local>
2024-01-29 09:37:52 +0100

    terminado

--- CalculadoraDeNota-mruperez/src/test/java/com/gsd/daw/prog/AppTest.java
+++ CalculadoraDeNota-mruperez/src/test/java/com/gsd/daw/prog/AppTest.java
@@ -1,6 +1,8 @@
 package com.gsd.daw.prog;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
@@ -8,12 +10,19 @@ import org.junit.jupiter.api.TestMethodOrder;
 @TestMethodOrder( MethodOrderer.DisplayName.class )
 public class AppTest 
 {
+	/*
+	 * Siempre funciona
+	 */
     @Test
     public void siempreFunciona()
     {
         assertTrue( true );
     }
-    
+    /*
+     * Comprueba que al introducir un tipo de nota de mas de un caracter de largo
+     * salta una excepcion
+     */
+    @DisplayName("Lanza excepcion por tipo de nota muy largo")
     @Test 
     public void excepcionPorLetraMuyLarga() throws IllegalArgumentException {
     	try {
@@ -24,7 +33,11 @@ public class AppTest
     		
     	}
     }
-    
+    /*
+     * Comprueba que al introducir un caracter en el tipo de nota
+     * que no sea 'A', 'C', 'P', o 'T' salte un a excepción
+     */
+    @DisplayName("Lanza excepcion por tipo de nota no valido")
     @Test 
     public void excepcionPorLetraNoValida() throws IllegalArgumentException {
     	try {
@@ -35,7 +48,11 @@ public class AppTest
 
     	}
     }
-    
+    /*
+     * Comprueba que al introducir un caracter en la nota
+     *  que no es numero salta excepción
+     */
+    @DisplayName("Lanza excepcion por numero no valido")
     @Test 
     public void excepcionPorNumeroNoValido() throws IllegalArgumentException {
     	try {
@@ -47,7 +64,10 @@ public class AppTest
     	}
     	
     }
-    
+    /*
+     * Comprueba que al introducir una nota mayor que 10 salta excepción
+     */
+    @DisplayName("Lanza excepcion por nota demasiado grande")
     @Test 
     public void excepcionPorNumeroMuyGrande() throws IllegalArgumentException {
     	try {
@@ -58,7 +78,11 @@ public class AppTest
 
     	}
     }
-    
+    /*
+     * Comprueba que se lanza una excepcion si a nota de asistencia es mayor
+     * que uno.
+     */
+    @DisplayName("Lanza excepcion por nota de asistencia demasiado grande")
     @Test 
     public void excepcionPorNotaAsistenciaMuyGrande() throws IllegalArgumentException {
     	try {
@@ -69,7 +93,11 @@ public class AppTest
 
     	}
     }
-    
+    /*
+     * Comprueba que se lanza una excepcion si a nota de actitud es mayor
+     * que uno.
+     */
+    @DisplayName("Lanza excepcion por nota de actitud demasiado grande")
     @Test 
     public void excepcionPorNotaActitudMuyGrande() throws IllegalArgumentException {
     	try {
@@ -81,8 +109,12 @@ public class AppTest
     	}
     }
     
+    /*
+     * Comprueba que guarda la nota bien
+     */
+    @DisplayName("Guarda la nota bien")
     @Test 
-    public void devuelveNota() {
+    public void guardaNota() {
     	Nota t = new Nota("C-10");
     	
     	double devolucion = t.getNota();
@@ -90,6 +122,10 @@ public class AppTest
     	assertEquals(10, devolucion);
     }
     
+    /*
+     * Comprueba que devuelve el tipo de nota correcto
+     */
+    @DisplayName("Devuelve el tipo de nota correcto")
     @Test 
     public void devuelveTipoDeNota() {
     	Nota t = new Nota("C-10");
@@ -99,6 +135,11 @@ public class AppTest
     	assertEquals('C', devolucion);
     }
     
+    /*
+     * Comprueba que lanza una excepción si no se 
+     * ha introducido ninguna nota de examen.
+     */
+    @DisplayName("Lanza excepcion si no hay nota de examen")
     @Test 
     public void excepcionPorFaltaDeNotaDeExamen() throws IllegalArgumentException {
     	Nota asistencia = new Nota("A-1");
@@ -116,6 +157,10 @@ public class AppTest
     	}
     }
     
+    /*
+     * Comprueba que se lanza una excepcion si no hay nota de practicas
+     */
+    @DisplayName("Lanza excepcion si no hay nota de practicas")
     @Test 
     public void lanzaExcepcionPorFaltaDeNotaDePractica() throws IllegalArgumentException {
     	Nota asistencia = new Nota("A-1");
@@ -133,6 +178,11 @@ public class AppTest
     	}
     }
     
+    /*
+     * Comprueba que lanza una excepción si hay mas de una nota de 
+     * asistencia
+     */
+    @DisplayName("Lanza excepcion si hay demasiadas notas de asistencia")
     @Test 
     public void lanzaExcepcionPorDemasiadasNotasAsistencia() throws IllegalArgumentException {
     	Nota asistencia1 = new Nota("A-1");
@@ -152,6 +202,11 @@ public class AppTest
     	}
     }
     
+    /*
+     * Comprueba que se devuelve una nota final correcta si los 
+     * argumentos son correctos
+     */
+    @DisplayName("Devuelve una nota final correcta")
     @Test 
     public void calculaNotaConArgumentosCorrectosBien() {
     	Nota asistencia = new Nota("A-1");
@@ -166,6 +221,11 @@ public class AppTest
     	assertEquals(7.2, e.getNotaFinal());
     }
     
+    /*
+     * Comprueba que se devuelve un cero como nota final
+     * si una nota de examen es menor que tres
+     */
+    @DisplayName("Devuelve cero si una nota de examen es menor que tres")
     @Test 
     public void comprobarQueLaNotaEsCeroSiUnExamenEsMenorQueTres() {
     	Nota asistencia = new Nota("A-1");
@@ -180,6 +240,11 @@ public class AppTest
     	assertEquals(0, e.getNotaFinal());
     }
     
+    /*
+     * Comprueba que se devuelve un cero como nota final
+     * si una nota de examen es menor que tres
+     */
+    @DisplayName("Devuelve cero si una nota de examen es menor que tres")
     @Test
     public void comprobarQueLaNotaEsCeroSiUnaPracticaEsMenorQueCuatro() {
     	Nota asistencia = new Nota("A-1");
@@ -193,7 +258,10 @@ public class AppTest
     	e.calcular();
     	assertEquals(0, e.getNotaFinal());
     }
-    
+    /*
+     * Comprueba que se lanza una excepcion si hay mas de una nota de actitud 
+     */
+    @DisplayName("Lanza excepcion si hay mas de una nota de actitud")
     @Test
     public void lanzaExcepcionPorDemasiadasNotasActitud() throws IllegalArgumentException {
     	Nota actitud1 = new Nota("T-1");
@@ -212,18 +280,34 @@ public class AppTest
 
     	}
     }
+    
+    /*
+     * Comprueba que la clase main funciona correctamente si los argumentos
+     * son los esperados 
+     */
+    @DisplayName("La clase main funciona con argumentos correctos")
     @Test
     public void funcionaClaseMainConArgsCorrectos() {
     	String[] args = {"C-5", "P-8", "A-1", "T-1"};
     	App.main(args);
     	assertTrue(true);
     }
+    
+    /*
+     * Comprueba que la clase main funciona con argumentos incorrectos
+     */
+    @DisplayName("La clase main funciona con argumentos incorrectos")
     @Test
     public void comprobarManejoDeExcepcionesClaseMain() {
     	String[] args = {"C-5", "P-8", "A-1", "T-x"};
     	App.main(args);
     	assertTrue(true);
     }
+    
+    /*
+     * Comprueba que devuelve cero si una nota de practicas es menor que cuatro
+     */
+    @DisplayName("Devuelve cero si una nota de practicas es menor que cuatro")
     @Test
     public void comprobarConNotaDeTeoriaMenorQueCuatro() {
     	Nota asistencia = new Nota("A-1");
@@ -237,6 +321,11 @@ public class AppTest
     	e.calcular();
     	assertEquals(0, e.getNotaFinal());
     }
+    
+    /*
+     * Comprueba que muestra error y se detiene si hay mas de veinte argumentos
+     */
+    @DisplayName("Muestra error y se detiene si hay demasiados argumentos")
     @Test
     public void comprobarCuandoHayDemasiadosArgumentos() {
     	String[] args = {"C-5", "P-8", "A-1", "T-1", "C-6", "P-8", "P-8", "C-9", "C-5", "C-5",
