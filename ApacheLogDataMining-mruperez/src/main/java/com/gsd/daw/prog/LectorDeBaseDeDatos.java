09b8e2b15cd2c32007c2de85e05ce9ed6bb8db2f
m.ruperez <m.ruperez@2DAW-12.BUINF.local>
2024-04-15 15:53:10 +0200

    cambios en el metodo extraer tabla
    es mejor devolver un arraylist que un array de strings

--- ApacheLogDataMining-mruperez/src/main/java/com/gsd/daw/prog/LectorDeBaseDeDatos.java
+++ ApacheLogDataMining-mruperez/src/main/java/com/gsd/daw/prog/LectorDeBaseDeDatos.java
@@ -1,13 +1,15 @@
 package com.gsd.daw.prog;
 
 import java.sql.Connection;
+import java.util.List;
+import java.util.ArrayList;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
 public class LectorDeBaseDeDatos {
-	private static final int TAMANIO_MAXIMO=10000; 
+	//private static final int TAMANIO_MAXIMO=10000; 
 	private static final int COLUMNAS=6; 
 	private Connection conn;
 	public LectorDeBaseDeDatos(Connection conexion) {
@@ -15,21 +17,23 @@ public class LectorDeBaseDeDatos {
 		
 	}
 	
-    public String[][] extraerTabla( ) throws Exception {
+    public ArrayList extraerTabla( ) throws Exception {
     	
         Statement stmt = this.conn.createStatement();
         ResultSet resultSet = stmt.executeQuery( "SELECT * FROM apache_log_tbl" );
-        String[][] resultadoConsulta = new String [TAMANIO_MAXIMO][6];
+        List <List> resultadoConsulta=new ArrayList<>();
         
         int i = 0;
         while ( resultSet.next() ) {
             try {
-            	resultadoConsulta[i][0] = resultSet.getString( "IP" ) ;
-            	resultadoConsulta[i][1] = resultSet.getString( "timestamp" ) ;
-            	resultadoConsulta[i][2] = resultSet.getString( "request" ) ;
-            	resultadoConsulta[i][3] = resultSet.getString( "result" ) ;
-            	resultadoConsulta[i][4] = resultSet.getString( "bytes" ) ;
-            	resultadoConsulta[i][5] = resultSet.getString( "ua" ) ;
+            	
+            	
+//            	resultadoConsulta[i][0] = resultSet.getString( "IP" ) ;
+//            	resultadoConsulta[i][1] = resultSet.getString( "timestamp" ) ;
+//            	resultadoConsulta[i][2] = resultSet.getString( "request" ) ;
+//            	resultadoConsulta[i][3] = resultSet.getString( "result" ) ;
+//            	resultadoConsulta[i][4] = resultSet.getString( "bytes" ) ;
+//            	resultadoConsulta[i][5] = resultSet.getString( "ua" ) ;
             } 
             catch(NumberFormatException e) {
 
@@ -40,16 +44,16 @@ public class LectorDeBaseDeDatos {
         stmt.close();
         conn.close();
         
-        if( resultadoConsulta.length == i ) {return resultadoConsulta;}
-        
-        String aux[][] = new String [i][6];
-        
-		for(int j = (resultadoConsulta.length-1); j >= 0; j--) {
-			if(resultadoConsulta[j][0] != null) {
-				aux[i-1]=resultadoConsulta[j];
-				i--;
-			}
-		}
-        return aux;
+//        if( resultadoConsulta.length == i ) {return resultadoConsulta;}
+//        
+//        String aux[][] = new String [i][6];
+//        
+//		for(int j = (resultadoConsulta.length-1); j >= 0; j--) {
+//			if(resultadoConsulta[j][0] != null) {
+//				aux[i-1]=resultadoConsulta[j];
+//				i--;
+//			}
+//		}
+//        return aux;
     }
 }
