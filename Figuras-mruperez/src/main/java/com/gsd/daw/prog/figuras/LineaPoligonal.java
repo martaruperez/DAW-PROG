e5d76daea1e3a3a8087c927326a438bc49091c6f
marta <marta@ASUS-MARTA>
2024-12-10 18:42:37 +0100

    clase LineaPoligonal hecha

--- Figuras-mruperez/src/main/java/com/gsd/daw/prog/figuras/LineaPoligonal.java
+++ Figuras-mruperez/src/main/java/com/gsd/daw/prog/figuras/LineaPoligonal.java
@@ -1,5 +1,34 @@
 package com.gsd.daw.prog.figuras;
 
 public class LineaPoligonal {
+	private Punto[] puntos;
+	private Stroke stroke;
 
+	public LineaPoligonal(Punto[] puntos) {
+		this.puntos = puntos;
+		this.stroke = new Stroke(new Color((byte)0,(byte)0,(byte)0),1);
+	}
+	
+	private String puntosToString() {
+		String s = "";
+		for(int i = 0; i < puntos.length; i++) {
+			s+=puntos[i].getX()+","+puntos[i].getY();
+	        if (i < puntos.length - 1) {
+	            s += " ";
+	        }
+		}
+		return s;
+	}
+	
+	public String toSvg() {
+		return "<polyline points=\""+puntosToString()+"\" "
+				+this.stroke.toSvg()+"/>";
+	}
+	
+	public void setStroke(Stroke stroke) throws IllegalArgumentException{
+		if(null == stroke) {
+			throw new IllegalArgumentException("ERROR: El stroke no puede ser nulo");
+		}
+		this.stroke = stroke;
+	}
 }
